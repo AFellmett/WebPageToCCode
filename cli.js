f8e8d5ee55c80de759dbee2940578faa4144fcc8
AFellmett <AFellmett@gmail.com>
2021-11-13 17:10:13 +0100

    Refactor: Moved to eta template engine

--- cli.js
+++ cli.js
@@ -5,8 +5,11 @@
  * Declare imports
  */
 import {existsSync, promises} from 'fs';
+import path from 'path';
+import { fileURLToPath } from 'url';
 import mime from 'mime';
 import yargs from 'yargs';
+import * as eta from 'eta';
 
 /**
  * Configure CLI Options
@@ -56,44 +59,11 @@ const argv = yargs(process.argv.splice(2))
  */
 const headerFileName = 'website.h';
 const framework = argv.framework;
-const templateDir = `template/${framework}/`;
+const templateDir = path.join(path.dirname(fileURLToPath(import.meta.url)),'template',framework);
 const sourceFileName = framework == 'arduino' ? 'website.cpp' : 'website.c';
-const copyright = `Copyright ${new Date().getFullYear()} ${argv.author}\n`;
-
-/**
- * Copy the template Header file and add the copyright statement
- *
- * @param {string} targetDir Target directory, where the C Files are going to be placed
- */
-async function createHeader(targetDir) {
-  if (targetDir == null) return;
-
-  try {
-    await promises.writeFile(`${targetDir}/${headerFileName}`, `/* ${copyright}`);
-    const data =
-      await promises.readFile(new URL(`${templateDir}${headerFileName}`, import.meta.url));
-    await promises.appendFile(`${targetDir}/${headerFileName}`, data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-
-/**
- *
- * @param {string} targetDir Target directory, where the C Files are going to be placed
- */
-async function createSourceFile(targetDir) {
-  if (targetDir == null) return;
-
-  try {
-    await promises.writeFile(`${targetDir}/${sourceFileName}`, `/* ${copyright}`);
-    const data =
-      await promises.readFile(new URL(`${templateDir}${sourceFileName}`, import.meta.url));
-    await promises.appendFile(`${targetDir}/${sourceFileName}`, data);
-  } catch (error) {
-    console.log(error);
-  }
-}
+const author = argv.author;
+const sourceDir = argv.source;
+const year = new Date().getFullYear();
 
 /** getFiles is used to get all webfiles of a directory recursivly.
  *
@@ -142,145 +112,8 @@ function formatName(fileName) {
   return fileName.toUpperCase().replace(/[^a-zA-Z0-9]/g, '_');
 }
 
-/** writeFileData is used to write a C style char array
- *  containing all bytes of a given file
- *
- * @param {string} targetDir Target directory, where the C Files are going to be placed
- * @param {string} rootDir is representing the root directory
- * which contains the files
- * @param {string} fileName the name of the file to transform into char array
- */
-async function writeFileData(targetDir, rootDir, fileName) {
-  if (rootDir == null || fileName == null) return;
-
-  const name = formatName(fileName);
-  const data = await promises.readFile(`${rootDir}/${fileName}`);
-  const length = data.length;
-
-  // adding a seperate const with the length of the array helps
-  // to optimize the runtime code later on
-  await promises.appendFile(`${targetDir}/${sourceFileName}`
-      , `const size_t ${name}_Length = ${length};\n`);
-  switch (framework) {
-    case 'esp-idf':
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `const char ${name}[] = {\n`);
-      break;
-    default:
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `const char ${name}[] PROGMEM = {\n`);
-      break;
-  }
-  // use the buffer to not perform the slice function on each
-  // iteration step
-  const buffer = data.slice(0, -1);
-  for (const byte of buffer) {
-    await promises.appendFile(`${targetDir}/${sourceFileName}`, `0x${byte.toString(16)}, `);
-  }
-  await promises.appendFile(`${targetDir}/${sourceFileName}`
-      , `0x${data[length-1].toString(16)}\n};\n\n`);
-}
-
-/** writeFunctions is used to create the functions
- *  used by server to serve the websites
- *
- * @param {string} targetDir Target directory, where the C Files are going to be placed
- * @param {string} fileName the name of the file which has to be served
- * @param {string} mimetype the mime type of the file
- */
-async function writeFunctions(targetDir, fileName, mimetype) {
-  if (fileName == null || mimetype == null) return;
-
-  const name = formatName(fileName);
-  switch (framework) {
-    case 'esp-idf':
-      // just build the structure with all informations
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `struct web_content st_${name} = {\n`);
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `  .length = ${name}_Length,\n`);
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `  .data = ${name},\n`);
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `  .compression = ${fileName.endsWith('.gz')},\n`);
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `  .content_type = "${mimetype}"\n};\n\n`);
-      break;
-    default:
-      await promises.appendFile(`${targetDir}/${sourceFileName}`, `void ${name}Page() {\n`);
-      if (fileName.endsWith('.gz')) {
-        await promises.appendFile(`${targetDir}/${sourceFileName}`
-            , '  server.sendHeader("Content-Encoding", "gzip");\n');
-      }
-      // the server.send_P() function does exactly what we want
-      // and also cares about the content-length header
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , `  server.send_P(200, "${mimetype}", ${name}, ${name}_Length);\n}\n\n`);
-      break;
-  }
-}
-
-/** writeRegisterFunction is used to setup the registerWebsite()
- *  function and link all entrypoints of the website
- *
- * @param {string} targetDir Target directory, where the C Files are going to be placed
- * @param {string[]} fileNames all files which have to be referenced
- */
-async function writeRegisterFunction(targetDir, fileNames) {
-  if (fileNames == null) return;
-
-  switch (framework) {
-    case 'esp-idf':
-      let register = 'void registerWebsite(httpd_handle_t server) {\n';
-      for (const file of fileNames) {
-        const name = formatName(file);
-        const fileNameWoCompression = file.split('.gz')[0];
-
-        if (fileNameWoCompression == 'index.html') {
-          register =
-            register.concat(`  httpd_register_uri_handler(server, &index_html_Page);\n`);
-          await promises.appendFile(`${targetDir}/${sourceFileName}`
-              , `httpd_uri_t index_html_Page = {\n`);
-          await promises.appendFile(`${targetDir}/${sourceFileName}`
-              , `  .uri       = "/",\n`);
-          await promises.appendFile(`${targetDir}/${sourceFileName}`
-              , '  .method    = HTTP_GET,\n');
-          await promises.appendFile(`${targetDir}/${sourceFileName}`
-              , '  .handler   = website_content_handler,\n');
-          await promises.appendFile(`${targetDir}/${sourceFileName}`
-              , `  .user_ctx  = &st_${name}\n};\n\n`);
-        }
-        register = register.concat(`  httpd_register_uri_handler(server, &${name}Page);\n`);
-
-        await promises.appendFile(`${targetDir}/${sourceFileName}`
-            , `httpd_uri_t ${name}Page = {\n`);
-        await promises.appendFile(`${targetDir}/${sourceFileName}`
-            , `  .uri       = "/${fileNameWoCompression}",\n`);
-        await promises.appendFile(`${targetDir}/${sourceFileName}`
-            , '  .method    = HTTP_GET,\n');
-        await promises.appendFile(`${targetDir}/${sourceFileName}`
-            , '  .handler   = website_content_handler,\n');
-        await promises.appendFile(`${targetDir}/${sourceFileName}`
-            , `  .user_ctx  = &st_${name}\n};\n\n`);
-      }
-      register = register.concat(`}\n\n`);
-      await promises.appendFile(`${targetDir}/${sourceFileName}`, register);
-      break;
-    default:
-      await promises.appendFile(`${targetDir}/${sourceFileName}`
-          , 'void registerWebsite() {\n');
-      for (const file of fileNames) {
-        const name = formatName(file);
-        const fileNameWoCompression = file.split('.gz')[0];
-        if (fileNameWoCompression == 'index.html') {
-          await promises.appendFile(`${targetDir}/${sourceFileName}`
-              , `  server.on("/", ${name}Page);\n`);
-        }
-        await promises.appendFile(`${targetDir}/${sourceFileName}`
-            , `  server.on("/${fileNameWoCompression}", ${name}Page);\n}\n`);
-      }
-      break;
-  }
+async function getContent(fileName) {
+  return promises.readFile(`${sourceDir}/${fileName}`);
 }
 
 /** Entry point
@@ -314,29 +147,22 @@ async function main(sourceDir, targetDir) {
   }
   // sourceDirectory and targetDirectory exists -> do the work
   try {
-    // Copy the Header file
-    await createHeader(targetDir);
+    // configure eta engine
+    eta.configure({
+      views: templateDir,
+      async: true,
+    })
+
+    // generate header file
+    const website_h = await eta.renderFileAsync(`./${headerFileName}.eta`, {author, year})
+    await promises.writeFile(`${targetDir}/${headerFileName}`, website_h);
 
     // Get a list of all files which are inside the sourceDirectory
     const files = await getFiles(sourceDir);
 
-    // Create and open a c source file
-    await createSourceFile(targetDir);
-
-    // Write the static information
-    // like warning of autogenerated content and include the header file
-
-    // Create a char array constant out of all files
-    for (const file of files) {
-      await writeFileData(targetDir, sourceDir, file.file);
-    }
-    // Create a function for each file to serve the matching char array
-    for (const file of files) {
-      await writeFunctions(targetDir, file.file, file.mime);
-    }
-    // Finally implement the registerWebsite()
-    // function which registers all files with corresponding functions
-    await writeRegisterFunction(targetDir, files.map((value) => value.file));
+    // generate source file
+    const website = await eta.renderFileAsync(`./${sourceFileName}.eta`, {author, files, formatName, getContent, year})
+    await promises.writeFile(`${targetDir}/${sourceFileName}`, website);
 
     return returnCode;
   } catch (error) {
